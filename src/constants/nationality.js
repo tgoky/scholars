24d189093e28f1859d1db967c025751c6ecd2890
tgoky <tgokyicloud6@gmail.com>
2023-11-09 16:13:31 +0100

    chore: remit

--- src/constants/nationality.js
+++ src/constants/nationality.js
@@ -1,25 +1,41 @@
 const NATIONALITY = [
-    {
-      key: '0',
-      text: 'Any Difficulty',
-      value: '0',
-    },
-    {
-      key: 'easy',
-      text: 'Easy',
-      value: 'easy',
-    },
-    {
-      key: 'medium',
-      text: 'Medium',
-      value: 'medium',
-    },
-    {
-      key: 'hard',
-      text: 'Hard',
-      value: 'hard',
-    },
+    { key: 'usa', text: 'United States', value: 'United States' },
+    { key: 'canada', text: 'Canada', value: 'Canada' },
+    { key: 'uk', text: 'United Kingdom', value: 'United Kingdom' },
+    { key: 'australia', text: 'Australia', value: 'Australia' },
+    { key: 'ethopia', text: 'Ethopia', value: 'Ethopia' },
+    { key: 'france', text: 'France', value: 'France' },
+    { key: 'italy', text: 'Italy', value: 'Italy' },
+    { key: 'spain', text: 'Spain', value: 'Spain' },
+    { key: 'netherlands', text: 'Netherlands', value: 'Netherlands' },
+    { key: 'belgium', text: 'Belgium', value: 'Belgium' },
+    { key: 'nigeria', text: 'Nigeria', value: 'Nigeria' },
+    { key: 'kenya', text: 'Kenya', value: 'Kenya' },
+    { key: 'sweden', text: 'Sweden', value: 'Sweden' },
+    { key: 'togo', text: 'Togo', value: 'Togo' },
+    { key: 'denmark', text: 'Denmark', value: 'Denmark' },
+    { key: 'mali', text: 'Mali', value: 'Mali' },
+    { key: 'ireland', text: 'Ireland', value: 'Ireland' },
+    { key: 'ghana', text: 'Ghana', value: 'Ghana' },
+    { key: 'portugal', text: 'Portugal', value: 'Portugal' },
+    { key: 'greece', text: 'Greece', value: 'Greece' },
+    { key: 'poland', text: 'Poland', value: 'Poland' },
+    { key: 'austria', text: 'Austria', value: 'Austria' },
+    { key: 'hungary', text: 'Hungary', value: 'Hungary' },
+    { key: 'rwanda', text: 'Rwanda', value: 'Rwanda' },
+    { key: 'russia', text: 'Russia', value: 'Russia' },
+    { key: 'china', text: 'China', value: 'China' },
+    { key: 'japan', text: 'Japan', value: 'Japan' },
+    { key: 'southkorea', text: 'South Korea', value: 'South Korea' },
+    { key: 'india', text: 'India', value: 'India' },
+    { key: 'brazil', text: 'Brazil', value: 'Brazil' },
+    { key: 'mexico', text: 'Mexico', value: 'Mexico' },
+    { key: 'argentina', text: 'Argentina', value: 'Argentina' },
+    { key: 'southafrica', text: 'South Africa', value: 'South Africa' },
+    { key: 'egypt', text: 'Egypt', value: 'Egypt' },
+  
+    // Add more countries as needed
   ];
   
-  export default  NATIONALITY;
+  export default NATIONALITY;
   
