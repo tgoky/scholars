58af1372873240d5e986a422f65a1e78e2d60b68
tgoky <tgokyicloud6@gmail.com>
2023-11-09 13:10:43 +0100

    chore: pre rollup

---
+++ src/components/Main/registration.js
@@ -0,0 +1,231 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Segment,
+  Item,
+  Dropdown,
+  Divider,
+  Button,
+  Message,
+  TextArea
+} from 'semantic-ui-react';
+
+import mindImg from '../../images/mind.svg';
+
+import {
+  CATEGORIES,
+  NUM_OF_QUESTIONS,
+  DIFFICULTY,
+  QUESTIONS_TYPE,
+  COUNTDOWN_TIME,
+} from '../../constants';
+import { shuffle } from '../../utils';
+
+import Offline from '../Offline';
+
+const Registration = ({ startQuiz }) => {
+  const [category, setCategory] = useState('0');
+  const [numOfQuestions, setNumOfQuestions] = useState('Male');
+  const [difficulty, setDifficulty] = useState('easy');
+  const [questionsType, setQuestionsType] = useState('0');
+  const [countdownTime, setCountdownTime] = useState({
+    hours: 0,
+    minutes: 120,
+    seconds: 0,
+  });
+  const [processing, setProcessing] = useState(false);
+  const [error, setError] = useState(null);
+  const [offline, setOffline] = useState(false);
+
+  const handleTimeChange = (e, { name, value }) => {
+    setCountdownTime({ ...countdownTime, [name]: value });
+  };
+
+  let allFieldsSelected = false;
+  if (
+    category &&
+    numOfQuestions &&
+    difficulty &&
+    questionsType &&
+    (countdownTime.hours || countdownTime.minutes || countdownTime.seconds)
+  ) {
+    allFieldsSelected = true;
+  }
+
+  const fetchData = () => {
+    setProcessing(true);
+
+    if (error) setError(null);
+
+    const API = `https://opentdb.com/api.php?amount=${numOfQuestions}&category=${category}&difficulty=${difficulty}&type=${questionsType}`;
+
+    fetch(API)
+      .then(respone => respone.json())
+      .then(data =>
+        setTimeout(() => {
+          const { response_code, results } = data;
+
+          if (response_code === 1) {
+            const message = (
+              <p>
+                The API doesn't have enough questions for your query. (Ex.
+                Asking for 50 Questions in a Category that only has 20.)
+                <br />
+                <br />
+                Please change the <strong>No. of Questions</strong>,{' '}
+                <strong>Difficulty Level</strong>, or{' '}
+                <strong>Type of Questions</strong>.
+              </p>
+            );
+
+            setProcessing(false);
+            setError({ message });
+
+            return;
+          }
+
+          results.forEach(element => {
+            element.options = shuffle([
+              element.correct_answer,
+              ...element.incorrect_answers,
+            ]);
+          });
+
+          setProcessing(false);
+          startQuiz(
+            results,
+            countdownTime.hours + countdownTime.minutes + countdownTime.seconds
+          );
+        }, 1000)
+      )
+      .catch(error =>
+        setTimeout(() => {
+          if (!navigator.onLine) {
+            setOffline(true);
+          } else {
+            setProcessing(false);
+            setError(error);
+          }
+        }, 1000)
+      );
+  };
+
+  if (offline) return <Offline />;
+
+  return (
+    <Container>
+      <Segment>
+        <Item.Group divided>
+          <Item>
+            <Item.Image src={mindImg} />
+            <Item.Content>
+              <Item.Header>
+                <h1>MasterCard Scholarships</h1>
+              </Item.Header>
+              {error && (
+                <Message error onDismiss={() => setError(null)}>
+                  <Message.Header>Error!</Message.Header>
+                  {error.message}
+                </Message>
+              )}
+              <Divider />
+              <Item.Meta>
+                <p>Student Legal First Name</p>
+                <TextArea
+                  
+                />
+                <br />
+                <p>Student Middle Name</p>
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="Select No. of Questions"
+                  header="Select No. of Questions"
+                  options={NUM_OF_QUESTIONS}
+                  value={numOfQuestions}
+                  onChange={(e, { value }) => setNumOfQuestions(value)}
+                  disabled={processing}
+                />
+                <br />
+                <p>Student Last Name</p>
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="Select No. of Questions"
+                  header="Select No. of Questions"
+                  options={NUM_OF_QUESTIONS}
+                  value={numOfQuestions}
+                  onChange={(e, { value }) => setNumOfQuestions(value)}
+                  disabled={processing}
+                />
+                <br />
+                <p>Gender</p>
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="M/F"
+                  header="Male"
+                  options={NUM_OF_QUESTIONS}
+                  value={numOfQuestions}
+                  onChange={(e, { value }) => setNumOfQuestions(value)}
+                  disabled={processing}
+                />
+                <br />
+                <p>Grade Level</p>
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="Select No. of Questions"
+                  header="Select No. of Questions"
+                  options={NUM_OF_QUESTIONS}
+                  value={numOfQuestions}
+                  onChange={(e, { value }) => setNumOfQuestions(value)}
+                  disabled={processing}
+                />
+                <br />
+                <p>Nationality</p>
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="Select No. of Questions"
+                  header="Select No. of Questions"
+                  options={NUM_OF_QUESTIONS}
+                  value={numOfQuestions}
+                  onChange={(e, { value }) => setNumOfQuestions(value)}
+                  disabled={processing}
+                />
+
+                <br />
+              </Item.Meta>
+              <Divider />
+              <Item.Extra>
+                <Button
+                  primary
+                  size="big"
+                  icon="play"
+                  labelPosition="left"
+                  content={processing ? 'Processing...' : 'Play Now'}
+                  onClick={fetchData}
+                  disabled={!allFieldsSelected || processing}
+                />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <br />
+    </Container>
+  );
+};
+
+Registration.propTypes = {
+  startQuiz: PropTypes.func.isRequired,
+};
+
+export default Registration;
---
+++ src/constants/numOfQuestions.js
@@ -0,0 +1,8 @@
+const NUM_OF_QUESTIONS = [
+  { key: 'male', text: 'Male', value: 'Male' },
+  { key: 'female', text: 'Female', value: 'Female' }
+];
+
+
+
+export default NUM_OF_QUESTIONS;
