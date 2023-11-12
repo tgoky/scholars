911547d8dc83b4e5449951288163eb68e0d4ffc7
tgoky <tgokyicloud6@gmail.com>
2023-11-12 14:41:09 +0100

    chore:up-to-date

--- src/components/Result/Stats.js
+++ src/components/Result/Stats.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React , {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Segment, Header, Button } from 'semantic-ui-react';
+import Acknowledge from './acknowledge';
 
 import ShareButton from '../ShareButton';
 import { calculateScore, calculateGrade, timeConverter } from '../../utils';
+
+
 
 const Stats = ({
   totalQuestions,
@@ -14,39 +17,59 @@ const Stats = ({
 }) => {
   const score = calculateScore(totalQuestions, correctAnswers);
   const { grade, remarks } = calculateGrade(score);
+  const [showMainContent, setShowMainContent] = useState(false);
   const { hours, minutes, seconds } = timeConverter(timeTaken);
 
+
+  const loadMain = () => {
+    setShowMainContent(true);
+  };
+
+
   return (
-    <Segment>
-      <Header as="h1" textAlign="center" block>
-        {remarks}
-      </Header>
-      <Header as="h2" textAlign="center" block>
-        Grade: {grade}
-      </Header>
-      <Header as="h3" textAlign="center" block>
-        Total Questions: {totalQuestions}
-      </Header>
-      <Header as="h3" textAlign="center" block>
-        Correct Answers: {correctAnswers}
-      </Header>
-      <Header as="h3" textAlign="center" block>
-        Your Score: {score}%
-      </Header>
-      <Header as="h3" textAlign="center" block>
-        Passing Score: 75%
-      </Header>
-      <Header as="h3" textAlign="center" block>
-        Time Taken:{' '}
-        {`${Number(hours)}h ${Number(minutes)}m ${Number(seconds)}s`}
-      </Header>
-      <div style={{ marginTop: 35 }}>
-       
-        <ShareButton />
-      </div>
-    </Segment>
+    showMainContent ? (
+      <Acknowledge />
+    ) : (
+      <Segment>
+        <Header as="h1" textAlign="center" block>
+          {remarks}
+        </Header>
+        <Header as="h2" textAlign="center" block>
+          Grade: {grade}
+        </Header>
+        <Header as="h3" textAlign="center" block>
+          Total Questions: {totalQuestions}
+        </Header>
+        <Header as="h3" textAlign="center" block>
+          Correct Answers: {correctAnswers}
+        </Header>
+        <Header as="h3" textAlign="center" block>
+          Your Score: {score}%
+        </Header>
+        <Header as="h3" textAlign="center" block>
+          Passing Score: 75%
+        </Header>
+        <Header as="h3" textAlign="center" block>
+          Time Taken:{' '}
+          {`${Number(hours)}h ${Number(minutes)}m ${Number(seconds)}s`}
+        </Header>
+        <div style={{ marginTop: 35 }}>
+         <Button
+          primary
+          content="Play Again"
+          onClick={loadMain}
+          size="big"
+          icon="redo"
+          labelPosition="left"
+          style={{ marginRight: 15, marginBottom: 8 }}
+        />
+
+          <ShareButton />
+        </div>
+      </Segment>
+    )
   );
-};
+    };  
 
 Stats.propTypes = {
   totalQuestions: PropTypes.number.isRequired,
@@ -57,3 +80,4 @@ Stats.propTypes = {
 };
 
 export default Stats;
+
---
+++ src/components/Result/acknowledge.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Container, Segment, Item, Message, Divider, Image } from 'semantic-ui-react';
+import thank from '../../images/thank.gif';  // Import your PNG logo
+
+import msLogo from '../../images/mastercardscholarslogo.png';
+
+const Acknowledge = () => {
+  const [error, setError] = useState(null);
+
+  return (
+    <Container>
+      <Segment>
+        <Item.Group divided>
+          <Item>
+            {/* Use the imported PNG image */}
+            <Item.Image src={msLogo} />
+            <Item.Content>
+              <Item.Header>
+                <h1>Thank you for successfully completing the Scholars Examination</h1>
+              </Item.Header>
+              {error && (
+                <Message error onDismiss={() => setError(null)}>
+                  <Message.Header>Error!</Message.Header>
+                  {error.message}
+                </Message>
+              )}
+              <Divider />
+              <Item.Meta>
+                <h> Your School or Center would receive an email 48 hours after examination </h>
+                {/* Use the GIF instead of an image and set the width and height */}
+                <Image src={thank} style={{ width: '250px', height: '250px',
+               margin: 'auto', // Center-align the image horizontally
+               display: 'block', // Ensure the margin works as expected
+            }} />
+              </Item.Meta>
+              <Divider />
+              <Item.Extra>
+                {/* Additional content goes here */}
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <br />
+    </Container>
+  );
+};
+
+export default Acknowledge;
